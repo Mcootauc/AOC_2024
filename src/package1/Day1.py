9818ae46a42a4916f4958edc34d88f9f551071bd
Mitchell Cootauco <Mcootauc@lion.lmu.edu>
2024-12-10 17:03:04 -0800

    Day1 Pt2 Final 2nd submit

--- src/package1/Day1.py
+++ src/package1/Day1.py
@@ -20,28 +20,26 @@ total_distance = sum(abs(a - b) for a, b in zip(list_1, list_2))
 print(total_distance) # Answer: 2378066
 
 # Day 1 Part 2:
+from collections import defaultdict
 
-# Initialize a similarity score and a hash map
+# Initialize a hash map with default value of 0 and a similarity score
+hash_map = defaultdict(int)
 similarity_score = 0
-hash_map = {}
 
 # Reads the content of the file Day1.txt
 with open('Day1.txt', 'r') as file:
     for line in file:
-        num1, num2 = map(int, line.split())
-        # Add number to hash map if it doesn't exist and iterate the count if it does
-        if (hash_map.get(num2) is None):
-            hash_map[num2] = 1
-        else:
-            hash_map[num2] += 1
+        _, num2 = map(int, line.split())
+        # Count how often each number appears in the right list
+        hash_map[num2] += 1
 
 # Reads the content of the file Day1.txt
 with open('Day1.txt', 'r') as file:
     for line in file:
-        num1, num2 = map(int, line.split())
+        num1, _ = map(int, line.split())
         # Checks if the number exists in the hash map, if it does, add the product of the number and the count to the similarity score
-        if (hash_map.get(num1) is not None):
-            similarity_score += hash_map[num1] * num1 
+        if hash_map[num1] is not None:
+            similarity_score += num1 * hash_map[num1]
         else:
             continue
 
