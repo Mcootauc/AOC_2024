7d40b419b62aedef291f7f7228c01dbd5bd56a83
Mitchell Cootauco <Mcootauc@lion.lmu.edu>
2024-12-28 21:35:01 -0800

    "Finished Day4 Part 2"

--- src/package1/Day4.py
+++ src/package1/Day4.py
@@ -1,3 +1,5 @@
+# Day 4 Part 1
+
 def check_horizontal(start, line):
     # Grabs the 4 letter string from left to right
     string = ''.join(line[start:start + 4])
@@ -58,3 +60,36 @@ with open('Day4.txt', 'r') as file:
                 word_count += 1
 
     print("(Part 1) XMAS is found the following number of times:", word_count) # The answer is 2547
+
+# Day 4 Part 2
+def check_diagonals(line_start, char_start, whole_list):
+    TL_BR_string = ''
+    # Grabs the 3 letter string from top left to bottom right
+    for i in range(3):
+        TL_BR_string += whole_list[line_start + i][char_start + i]
+
+    TR_BL_string = ''
+    # Grabs the 3 letter string from top right to bottom left
+    for i in range(3):
+        TR_BL_string += whole_list[line_start + i][char_start - i + 2]
+
+    # Checks if both diagonal strings are either MAS or SAM
+    if ((whole_list[line_start][char_start] == 'M' and TL_BR_string == 'MAS') or \
+        (whole_list[line_start][char_start] == 'S' and TL_BR_string == 'SAM')) and \
+        ((whole_list[line_start][char_start + 2] == 'M' and TR_BL_string == 'MAS') or \
+        (whole_list[line_start][char_start + 2] == 'S' and TR_BL_string == 'SAM')):
+        return True
+    
+    return False
+
+with open('Day4.txt', 'r') as file:
+    word_count = 0
+
+    # Reads the file and creates a 2D list
+    whole_list = [[char for char in line if char != '\n'] for line in file]
+    for i in range(len(whole_list)):
+        for j in range(len(whole_list[i])):
+            if i < len(whole_list) - 2 and j < len(whole_list[i]) - 2 and (whole_list[i][j] == 'M' or whole_list[i][j] == 'S') and check_diagonals(i, j, whole_list):
+                word_count += 1
+    
+    print("(Part 2) MAS is found the following number of times:", word_count) # The answer is 1
